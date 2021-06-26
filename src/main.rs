7db2b9e95a43c35f5aa574993fbcefc4f1745d41
Pyros <zack.c.dell@gmail.com>
2021-06-26 11:21:44 -0700

    :cry: in rust

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ fn main() {
     let port = 8080;
     let socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
 
+	
+
     /*
     *	Essentially, what I need to figure out here is both how to write decent Rust
     *	and how to use the networking libriary which is a decent library ngl
@@ -22,18 +24,21 @@ fn main() {
     *	Here, what I am testing is if I can have a loop for looking for new connections
     *	and then "routing" them to something, or rather, an actual TCP socket connection I guess?
     */
-    let _inputStream = std::net::TcpListener::bind(socket).unwrap();
+    let _input_stream = std::net::TcpListener::bind(socket).unwrap();
 
-    // _inputStream.
+    _input_stream.accept().unwrap();
 
-    for packet in _inputStream.incoming() {
-        match packet {
-            Ok(mut data) => {
+    for packet in _input_stream.incoming() 
+	{
+        match packet 
+		{
+            Ok(mut data) => 
+			{
                 println!("Connection Succeeded");
 
                 let mut datareceived = String::new();
 
-                data.read_to_string(&mut datareceived);
+                data.read_to_string(&mut datareceived).unwrap();
 
                 let peeraddr = data.peer_addr().unwrap();
 
@@ -44,7 +49,8 @@ fn main() {
                 let player = connection_routing::Player::new(peersocket, "name", 0);
             }
 
-            Err(e) => {
+            Err(e) => 
+			{
                 println!("ERROR! Connection Failed");
             }
         }
