f256f7ce5f28ce97ea65caa34a126ec6ac97a2c7
PyroFlareX <31327816+PyroFlareX@users.noreply.github.com>
2021-06-28 17:15:58 -0700

    Maybe figured something out

--- src/connection_routing.rs
+++ src/connection_routing.rs
@@ -1,47 +1,8 @@
 
-#[derive(Debug, Hash)]
-pub struct Player {
-	socket: std::net::SocketAddr,
-	name: String,
-	id: u64,
-}
 
-#[derive(Debug, Hash, PartialEq, Eq)]
-pub struct PeerID 
+pub struct clientData
 {
-	name: String,
-}
+	id: u32,
 
-impl Player 
-{
-	pub fn new(socket: std::net::SocketAddr, name: &str, id: u64) -> Self 
-	{
-		let string : String = name.into();
-		Self 
-		{
-			socket, 
-			name : string, 
-			id 
-		}
-	}
-}
-
-impl PeerID
-{
-	pub fn new() -> Self
-	{
-		Self
-		{
-			name :	"name".to_string(),
-		}
-	}
-	
-	pub fn newWithName(name: String) -> Self
-	{
-		// let name2 = name.to_owned();
-		Self
-		{
-			name
-		}
-	}
+	name : Option<String>,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use std::borrow::BorrowMut;
 use std::io::Read;
 use std::collections::HashMap;
+
+use enet::Enet;
 
 #[allow(non_snake_case)]
 #[allow(unused_variables)]
@@ -7,7 +10,6 @@ use std::collections::HashMap;
 #[allow(dead_code)]
 
 mod connection_routing;
-use crate::connection_routing::{PeerID, Player};
 
 fn main() 
 {
@@ -18,7 +20,7 @@ fn main()
 	let port = 6969;
 	let socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
 
-	let mut user_map = HashMap::<connection_routing::PeerID, connection_routing::Player>::new();
+	let mut peervec = Vec::new();
 
 	/*
 	*	Essentially, what I need to figure out here is both how to write decent Rust
@@ -27,9 +29,19 @@ fn main()
 	*	Here, what I am testing is if I can have a loop for looking for new connections
 	*	and then "routing" them to something, or rather, an actual TCP socket connection I guess?
 	*/
+
+	//let clientdata: json::Array;
+	
+	//let enetapi = enet::Enet::new().unwrap();
+	/*let server = enetapi.create_host(None, 32, 
+		enet::ChannelLimit::Limited(2), enet::BandwidthLimit::Unlimited, 
+		enet::BandwidthLimit::Unlimited).unwrap();
+
+	*/
+
 	let _input_stream = std::net::TcpListener::bind(socket).unwrap();
 
-	_input_stream.accept().unwrap();
+
 
 	for packet in _input_stream.incoming() 
 	{
@@ -41,22 +53,30 @@ fn main()
 
 				let mut datareceived = String::new();
 				let peeraddr = data.peer_addr().unwrap();
-				let peersocket = std::net::SocketAddr::new(peeraddr.ip(), port);
+				//let peersocket = std::net::SocketAddr::new(peeraddr.ip(), port);
 
 				data.read_to_string(&mut datareceived).unwrap();
 
 				println!("{}", datareceived);
 
-				let player = connection_routing::Player::new(peersocket, "name", 0);
+				
+				peervec.push(std::net::TcpStream::connect(peeraddr).unwrap());
+
+				//let player = connection_routing::Player::new(peersocket, "name", 0);
 				
 																//For some godforsaken reason this is a closure?
-				user_map.entry(PeerID::newWithName(get_user_id(datareceived))).or_insert_with(|| player);
+				//user_map.entry(PeerID::newWithName(get_user_id(datareceived))).or_insert_with(|| player);
 			}
 
 			Err(_e) => 
 			{
 				println!("ERROR! Connection Failed");
 			}
+		}
+
+		for peer in peervec
+		{
+			// peer
 		}
 	}
 }
