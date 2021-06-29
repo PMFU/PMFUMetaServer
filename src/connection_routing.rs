56e69ae8689bc9662e26bf0323172996f6d5fe52
Pyros <zack.c.dell@gmail.com>
2021-06-28 23:42:46 -0700

    Communication is dread

--- src/connection_routing.rs
+++ src/connection_routing.rs
@@ -1,8 +1,30 @@
 
 
+pub fn get_user_id(str: String) -> String 
+{
+	let mut string = String::new();
+
+	let whitespace = " \n\t";
+
+	for character in str.chars()
+	{
+		if whitespace.contains(character) 
+		{
+			return string;
+		} 
+		else 
+		{
+			string.push(character);
+		}
+	}
+
+	string
+}
+
 pub struct ClientData
 {
 	id: u32,
 
 	name : Option<String>,
 }
+
--- src/main.rs
+++ src/main.rs
@@ -78,27 +78,6 @@ fn main()
 	}
 }
 
-fn get_user_id(str: String) -> String 
-{
-	let mut string = String::new();
-
-	let whitespace = " \n\t";
-
-	for character in str.chars()
-	{
-		if whitespace.contains(character) 
-		{
-			return string;
-		} 
-		else 
-		{
-			string.push(character);
-		}
-	}
-
-	string
-}
-
 
 fn xyz()
 {
