224de48870e110e0a6f7f39254c61d6c07e45e8d
Pyros <zack.c.dell@gmail.com>
2021-06-29 13:16:10 -0700

    Pushing for help

--- src/connection_routing.rs
+++ src/connection_routing.rs
@@ -20,21 +20,24 @@ pub struct ClientData {
     name: Option<String>,
 }
 
+fn xyz() {
+    let port = 6969;
+    let addr = enet::Address::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
+    let socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
 
-fn xyz()
-{
-	let port = 6969;
-	let addr = enet::Address::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
-	let socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
+    let enetapi = enet::Enet::new().unwrap();
 
+    let mut client = enetapi
+        .create_host::<u32>(
+            None,
+            4,
+            enet::ChannelLimit::Maximum,
+            enet::BandwidthLimit::Unlimited,
+            enet::BandwidthLimit::Unlimited,
+        )
+        .unwrap();
 
-	let enetapi = enet::Enet::new().unwrap();
+    client.connect(&addr, 4, 1).unwrap();
 
-	let mut client = enetapi.create_host::<u32>(None, 4, enet::ChannelLimit::Maximum, enet::BandwidthLimit::Unlimited, enet::BandwidthLimit::Unlimited).unwrap();
-
-	client.connect(&addr, 4, 1).unwrap();
-
-	let e = client.service(1000).unwrap();
-
-    
+    let e = client.service(1000).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -10,69 +10,79 @@ use enet::Event;
 mod connection_routing;
 
 fn main() {
-	println!("Hello, world!");
+    println!("Hello, world!");
 
-	//  Actually start the basic testing
+    //  Actually start the basic testing
 
-	let port = 6969;
-	let addr = enet::Address::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
-	let socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
+    let port = 6969;
+    let addr = enet::Address::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
+    let _socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
 
+    let enetapi = enet::Enet::new().unwrap();
 
-	let enetapi = enet::Enet::new().unwrap();
+    let mut server = enetapi
+        .create_host::<u32>(
+            Some(&addr),
+            100,
+            enet::ChannelLimit::Maximum,
+            enet::BandwidthLimit::Unlimited,
+            enet::BandwidthLimit::Unlimited,
+        )
+        .unwrap();
 
-	let mut server = enetapi.create_host::<u32>(Some(&addr), 100, enet::ChannelLimit::Maximum, 
-											enet::BandwidthLimit::Unlimited, enet::BandwidthLimit::Unlimited).unwrap();
+    /*
+    	*	Essentially, what I need to figure out here is both how to write decent Rust
+    	*	and how to use the networking libriary which is a decent library ngl
+    	*
+    	*	Here, what I am testing is if I can have a loop for looking for new connections
+    	*	and then "routing" them to something, or rather, an actual TCP socket connection I guess?
+    	*/
 
+    //let clientdata: json::Array;
 
-	/*
-		*	Essentially, what I need to figure out here is both how to write decent Rust
-		*	and how to use the networking libriary which is a decent library ngl
-		*
-		*	Here, what I am testing is if I can have a loop for looking for new connections
-		*	and then "routing" them to something, or rather, an actual TCP socket connection I guess?
-		*/
+    let mut id = 0;
 
-	//let clientdata: json::Array;
-
-	let mut id = 0;
-
-	do_update(&mut server, &mut id);
-
+    do_update(&mut server, &mut id);
 }
 
-fn do_update(server : &mut enet::Host::<u32>, id: &mut u32)
-{
-	let event = server.service(1000);
+fn do_update(server: &mut enet::Host<u32>, id: &mut u32) {
+    let event = server.service(1000).unwrap();
+	
+    match &mut event.unwrap() {
+        Event::Connect(peer) => {
+            println!(
+                "Connection from peer! IP: {}",
+                peer.address().ip().to_string()
+            );
 
-	match &mut event.unwrap().unwrap()
-	{
-		Event::Connect(peer) =>
-		{
-			println!("Connection from peer! IP: {}", peer.address().ip().to_string());
+            let id_local = id.to_owned();
+            id.checked_add(1);
 
-			let id_local = *id;
-			*id += 1;
+            let addr = peer.address();
 
-			server.connect(&peer.address(), 4, id_local);
-			
-			
-		}
+            server.connect(&addr, 4, id_local);
+        }
 
-		Event::Disconnect(peer, id) =>
-		{
-			peer.disconnect(*id);
-		}
+        Event::Disconnect(peer, id) => {
+            let local_id = id.to_owned();
+            peer.disconnect(local_id);
+        }
 
-		Event::Receive{sender, channel_id, packet} =>
-		{
-			let mut str = String::new();
-			packet.data().read_to_string(&mut str);
-			
-			// sender.address().ip().to_string()
+        Event::Receive {
+            sender,
+            channel_id,
+            packet,
+        } => {
+            let mut str = String::new();
+            packet.data().read_to_string(&mut str);
 
-			println!("Data: {} | from IP: {}", str, sender.address().ip().to_string());
-		}
+            // sender.address().ip().to_string()
 
-	}
+            println!(
+                "Data: {} | from IP: {}",
+                str,
+                sender.address().ip().to_string()
+            );
+        }
+    };
 }
