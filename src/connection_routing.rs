73c169ef183b1f9a8d205b87267897dac0ef69d4
Pyros <zack.c.dell@gmail.com>
2021-06-28 23:59:53 -0700

    Fix warnings

--- src/connection_routing.rs
+++ src/connection_routing.rs
@@ -1,30 +1,21 @@
+pub fn get_user_id(str: String) -> String {
+    let mut string = String::new();
 
+    let whitespace = " \n\t";
 
-pub fn get_user_id(str: String) -> String 
-{
-	let mut string = String::new();
+    for character in str.chars() {
+        if whitespace.contains(character) {
+            return string;
+        } else {
+            string.push(character);
+        }
+    }
 
-	let whitespace = " \n\t";
-
-	for character in str.chars()
-	{
-		if whitespace.contains(character) 
-		{
-			return string;
-		} 
-		else 
-		{
-			string.push(character);
-		}
-	}
-
-	string
+    string
 }
 
-pub struct ClientData
-{
-	id: u32,
+pub struct ClientData {
+    id: u32,
 
-	name : Option<String>,
+    name: Option<String>,
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,99 @@
+//use std::collections::HashMap;
 use std::io::{Read, Write};
-use std::collections::HashMap;
 
 #[allow(non_snake_case)]
 #[allow(unused_variables)]
 #[allow(array_into_iter)]
 #[allow(dead_code)]
-
 mod connection_routing;
 
-fn main() 
-{
-	println!("Hello, world!");
+fn main() {
+    println!("Hello, world!");
 
-	//  Actually start the basic testing
+    //  Actually start the basic testing
 
-	let port = 6969;
-	let socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
+    let port = 6969;
+    let socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
 
-	let mut peervec = Vec::new();
+    let mut peervec = Vec::new();
 
-	/*
-	*	Essentially, what I need to figure out here is both how to write decent Rust
-	*	and how to use the networking libriary which is a decent library ngl
-	*
-	*	Here, what I am testing is if I can have a loop for looking for new connections
-	*	and then "routing" them to something, or rather, an actual TCP socket connection I guess?
-	*/
+    /*
+    	*	Essentially, what I need to figure out here is both how to write decent Rust
+    	*	and how to use the networking libriary which is a decent library ngl
+    	*
+    	*	Here, what I am testing is if I can have a loop for looking for new connections
+    	*	and then "routing" them to something, or rather, an actual TCP socket connection I guess?
+    	*/
 
-	//let clientdata: json::Array;
-	
-	//let enetapi = enet::Enet::new().unwrap();
-	/*let server = enetapi.create_host(None, 32, 
-		enet::ChannelLimit::Limited(2), enet::BandwidthLimit::Unlimited, 
-		enet::BandwidthLimit::Unlimited).unwrap();
+    //let clientdata: json::Array;
 
-	*/
+    //let enetapi = enet::Enet::new().unwrap();
+    /*let server = enetapi.create_host(None, 32,
+        enet::ChannelLimit::Limited(2), enet::BandwidthLimit::Unlimited,
+        enet::BandwidthLimit::Unlimited).unwrap();
 
-	let _input_stream = std::net::TcpListener::bind(socket).unwrap();
+    */
 
+    let _input_stream = std::net::TcpListener::bind(socket).unwrap();
 
+    for packet in _input_stream.incoming() {
+        match packet {
+            Ok(mut data) => {
+                println!("Connection Succeeded");
 
-	for packet in _input_stream.incoming() 
-	{
-		match packet 
-		{
-			Ok(mut data) => 
-			{
-				println!("Connection Succeeded");
+                let mut datareceived = String::new();
+                let peeraddr = data.peer_addr().unwrap();
+                //let peersocket = std::net::SocketAddr::new(peeraddr.ip(), port);
 
-				let mut datareceived = String::new();
-				let peeraddr = data.peer_addr().unwrap();
-				//let peersocket = std::net::SocketAddr::new(peeraddr.ip(), port);
+                data.read_to_string(&mut datareceived).unwrap();
 
-				data.read_to_string(&mut datareceived).unwrap();
+                println!("{}", datareceived);
 
-				println!("{}", datareceived);
+                let stream = std::net::TcpStream::connect(peeraddr).unwrap();
+                peervec.push(stream);
 
-				let mut stream = std::net::TcpStream::connect(peeraddr).unwrap();
-				peervec.push(stream);
+                //let player = connection_routing::Player::new(peersocket, "name", 0);
 
-				//let player = connection_routing::Player::new(peersocket, "name", 0);
-				
-																//For some godforsaken reason this is a closure?
-				//user_map.entry(PeerID::newWithName(get_user_id(datareceived))).or_insert_with(|| player);
-			}
+                //For some godforsaken reason this is a closure?
+                //user_map.entry(PeerID::newWithName(get_user_id(datareceived))).or_insert_with(|| player);
+            }
 
-			Err(_e) => 
-			{
-				println!("ERROR! Connection Failed");
-			}
-		}
+            Err(_e) => {
+                println!("ERROR! Connection Failed");
+            }
+        }
 
-		for peer in peervec.iter_mut()
-		{
-			peer.write("buf".as_bytes()).unwrap();
-		}
-	}
+        for peer in peervec.iter_mut() {
+            peer.write("buf".as_bytes()).unwrap();
+        }
+    }
+
+    for peer in peervec.iter_mut() {
+        let mut string = String::new();
+        peer.read_to_string(&mut string);
+
+        println!("{}", string);
+
+        peer.write("buf".as_bytes()).unwrap();
+    }
 }
 
-
+/*
 fn xyz()
 {
-	let port = 6969;
-	let socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
+    let port = 6969;
+    let socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
 
-	let mut x = std::net::TcpStream::connect(socket).unwrap();
+    let mut x = std::net::TcpStream::connect(socket).unwrap();
 
-	loop
-	{
-		let mut s = String::new();
-		x.read_to_string(&mut s).unwrap();
+    loop
+    {
+        let mut s = String::new();
 
-		println!("{}", s);
-	}
-}
+
+
+        x.read_to_string(&mut s).unwrap();
+
+        println!("{}", s);
+    }
+}*/
