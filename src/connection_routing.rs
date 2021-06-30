ac28f53a7909046d9debf377e04bfad7a800b361
Pyros <zack.c.dell@gmail.com>
2021-06-29 20:46:36 -0700

    Clean up a ton and start game selection stuff

--- src/connection_routing.rs
+++ src/connection_routing.rs
@@ -1,43 +1,93 @@
-pub fn get_user_id(str: String) -> String {
-    let mut string = String::new();
+#[allow(non_snake_case)]
+#[allow(unused_variables)]
+#[allow(array_into_iter)]
+#[allow(dead_code)]
+use std::collections::HashMap;
 
-    let whitespace = " \n\t";
-
-    for character in str.chars() {
-        if whitespace.contains(character) {
-            return string;
-        } else {
-            string.push(character);
-        }
-    }
-
-    string
-}
+use enet::Packet;
+use json::JsonValue;
 
 pub struct ClientData {
-    id: u32,
+	id: u32,
 
-    name: Option<String>,
+	name: Option<String>,
 }
 
-fn client_example() {
-    let port = 6969;
-    let addr = enet::Address::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
-    let socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
+pub fn send_game_list_packet(games: HashMap<u32, Game>, client: &mut enet::Peer<u32>) {
+	let mut packet = JsonValue::new_array();
 
-    let enetapi = enet::Enet::new().unwrap();
+	for (id, game) in games {
+		let mut gamejson = JsonValue::new_object();
 
-    let mut client = enetapi
-        .create_host::<u32>(
-            None,
-            4,
-            enet::ChannelLimit::Maximum,
-            enet::BandwidthLimit::Unlimited,
-            enet::BandwidthLimit::Unlimited,
-        )
-        .unwrap();
+		gamejson["lobbyname"] = JsonValue::String(game.lobby_name);
+		gamejson["checksum"] = JsonValue::String(game.checksum);
 
-    client.connect(&addr, 4, 1).unwrap();
+		packet[json::stringify(id)] = gamejson.into();
+	}
 
-    let e = client.service(1000).unwrap();
+	let str = packet.pretty(1);
+
+	let data_packet = Packet::new(str.as_bytes(), enet::PacketMode::ReliableSequenced).unwrap();
+
+	client.send_packet(data_packet, 0).unwrap();
+}
+
+pub struct Game {
+	id: u32,
+
+	host_ip: std::net::Ipv4Addr,
+
+	lobby_name: String,
+	password: Option<String>,
+
+	checksum: String,
+}
+
+impl Game {
+	pub fn new(host_ip: std::net::Ipv4Addr, lobby_name: String, password: Option<String>) -> Self {
+		let generated_id;
+		generated_id = 321684210;
+
+		let chsum = "1111".to_owned();
+
+		Self {
+			id: generated_id,
+			host_ip,
+			lobby_name,
+			password,
+			checksum: chsum,
+		}
+	}
+
+	pub fn serialize(self) -> String {
+		let mut string = String::new();
+
+		string.push_str(self.lobby_name.as_str());
+		string.push('\n');
+		string.push_str(self.checksum.as_str());
+
+		string
+	}
+
+	pub fn get_ip(self) -> std::net::Ipv4Addr {
+		self.host_ip
+	}
+}
+
+//Other stuff
+
+pub fn get_user_id(str: String) -> String {
+	let mut string = String::new();
+
+	let whitespace = " \n\t";
+
+	for character in str.chars() {
+		if whitespace.contains(character) {
+			return string;
+		} else {
+			string.push(character);
+		}
+	}
+
+	string
 }
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@
 #[allow(unused_variables)]
 #[allow(array_into_iter)]
 #[allow(dead_code)]
-
 mod connection_routing;
-//use std::collections::HashMap;
-use std::io::{Read, Write, stdout};
 
-use enet::Event;
+use std::{collections::HashMap, io::{stdout, Read, Write}, ops::Index};
+
+use connection_routing::Game;
+use enet::{Event, Packet};
 
 fn main() {
 	println!("Hello, world!");
@@ -15,18 +15,21 @@ fn main() {
 	//  Actually start the basic testing
 
 	let port = 6969;
-	let addr = enet::Address::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
-	let _socket = std::net::SocketAddrV4::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
+	let ipaddr = std::net::Ipv4Addr::new(127, 0, 0, 1);
+	let local_addr = enet::Address::new(ipaddr, port);
 
 	stdout().flush().unwrap();
 
 	let enetapi = enet::Enet::new().unwrap();
 
+	//Server init
+
 	let max_peers_count = 128;
 
+	//Create a server on the localhost
 	let mut server = enetapi
 		.create_host::<u32>(
-			Some(&addr),
+			Some(&local_addr),
 			max_peers_count,
 			enet::ChannelLimit::Maximum,
 			enet::BandwidthLimit::Unlimited,
@@ -34,30 +37,29 @@ fn main() {
 		)
 		.unwrap();
 
-	/*
-		*	Essentially, what I need to figure out here is both how to write decent Rust
-		*	and how to use the networking libriary which is a decent library ngl
-		*
-		*	Here, what I am testing is if I can have a loop for looking for new connections
-		*	and then "routing" them to something, or rather, an actual TCP socket connection I guess?
-		*/
-
 	//let clientdata: json::Array;
 
+	//Data Hash Maps
+	//let mut client_map = HashMap::<u32, ClientData>::new();
+	let mut game_map = HashMap::<u32, Game>::new();
+
+	//
+	//for mut peer in server.peers() {}
+
+	game_map.insert(0, Game::new(ipaddr, "lobby_name".to_owned(), None));
+
+	//Start loop
 	let mut id = 0;
-	
+
 	loop {
-		do_update(&mut server, &mut id);
-		
-		
+		do_update(&mut server, &mut id, &mut game_map);
 	}
 }
 
-fn do_update(server: &mut enet::Host<u32>, id: &mut u32) {
+fn do_update(server: &mut enet::Host<u32>, id: &mut u32, game_map: &mut HashMap<u32, Game>) {
 	let event = server.service(1000).unwrap();
 
-	if event.is_none()
-	{
+	if event.is_none() {
 		return;
 	}
 
@@ -96,4 +98,63 @@ fn do_update(server: &mut enet::Host<u32>, id: &mut u32) {
 			);
 		}
 	};
+}
+
+//Client Example
+
+fn client_example() {
+	let port = 6969;
+	let remote_addr = enet::Address::new(std::net::Ipv4Addr::new(127, 0, 0, 1), port);
+
+	let enetapi = enet::Enet::new().unwrap();
+
+	//Create a connector (DOESN'T NEED IP) | Connects to server over network
+	let mut client = enetapi
+		.create_host::<u32>(
+			None,
+			1,
+			enet::ChannelLimit::Maximum,
+			enet::BandwidthLimit::Unlimited,
+			enet::BandwidthLimit::Unlimited,
+		)
+		.unwrap();
+
+	client.connect(&remote_addr, 1, 1).unwrap();
+
+	let mut id = 0;
+
+	let mut game_map = HashMap::<u32, Game>::new();
+
+	loop {
+		do_update(&mut client, &mut id, &mut game_map);
+
+		let mut input = String::new();
+		match std::io::stdin().read_line(&mut input) {
+			Ok(n) => {
+				println!("{} bytes read", n);
+				println!("{}", input);
+			}
+			Err(error) => println!("error: {}", error),
+		}
+
+		input.truncate(input.len() - 2);
+
+		if input.is_empty() || input == "STOP" {
+			break;
+		}
+
+		for mut peer in client.peers().into_iter() {
+			peer.send_packet(
+				Packet::new(input.as_bytes(), enet::PacketMode::ReliableSequenced).unwrap(),
+				0,
+			)
+			.unwrap();
+		}
+	}
+
+	println!("Disconnected!");
+
+	for mut peer in client.peers().into_iter() {
+		peer.disconnect(*peer.data().unwrap());
+	}
 }
